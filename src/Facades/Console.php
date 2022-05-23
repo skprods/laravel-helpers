e2113171546508915b71b91f13025833a7ed99b6
Pavel Sklyar <ozpavel@yandex.ru>
2022-05-23 19:16:29 +0300

    В ConsoleOutput добавлены методы для вывода кастомизированного сообщения и таблицы

--- src/Facades/Console.php
+++ src/Facades/Console.php
@@ -10,7 +10,9 @@ use Symfony\Component\Console\Helper\ProgressBar;
  * @method static info(string $message)
  * @method static comment(string $message)
  * @method static error(string $message)
+ * @method static message(string $message, string $foreground = null, string $background = null, array $options = [])
  * @method static ProgressBar bar(int $max = 0, float $minSecondsBetweenRedraws = 1 / 25)
+ * @method static table(array $headers, $rows, $tableStyle = 'default', array $columnStyles = [])
  *
  * @see ConsoleOutput
  */
--- src/Handlers/ConsoleOutput.php
+++ src/Handlers/ConsoleOutput.php
@@ -2,7 +2,10 @@
 
 namespace SKprods\LaravelHelpers\Handlers;
 
+use Illuminate\Contracts\Support\Arrayable;
 use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableStyle;
 use Symfony\Component\Console\Output\ConsoleOutput as BaseConsoleOutput;
 
 class ConsoleOutput
@@ -35,9 +38,63 @@ class ConsoleOutput
         $this->output->writeln($styled);
     }
 
+    /**
+     * Кастомизированное сообщение
+     *
+     * @param string|null $foreground   - цвет текста
+     * @param string|null $background   - цвет фона
+     * @param array $options            - дополнительные опции (bold, underscore, blink, reverse, conceal)
+     */
+    public function message(string $message, string $foreground = null, string $background = null, array $options = [])
+    {
+        $styles = [];
+
+        if ($foreground) {
+            $styles[] = "fg=$foreground";
+        }
+
+        if ($background) {
+            $styles[] = "bg=$background";
+        }
+
+        if (!empty($options)) {
+            $styles[] = "options=" . implode(',', $options);
+        }
+
+        $style = implode(';', $styles);
+        $styledMessage = empty($style) ? $message : "<$style>$message</>";
+
+        $this->output->writeln($styledMessage);
+    }
+
     public function bar(int $max = 0, float $minSecondsBetweenRedraws = 1 / 25): ProgressBar
     {
         return new ProgressBar($this->output, $max, $minSecondsBetweenRedraws);
+    }
+
+    /**
+     * Вывод таблицы в консоль
+     *
+     * @param array $headers                - заголовки таблицы
+     * @param Arrayable|array $rows         - строки таблицы
+     * @param TableStyle|string $tableStyle - кастомный стиль таблицы
+     * @param array $columnStyles           - кастомные стили для колонок в формате [columnIndex => TableStyle]
+     */
+    public function table(array $headers, $rows, $tableStyle = 'default', array $columnStyles = [])
+    {
+        $table = new Table($this->output);
+
+        if ($rows instanceof Arrayable) {
+            $rows = $rows->toArray();
+        }
+
+        $table->setHeaders($headers)->setRows($rows)->setStyle($tableStyle);
+
+        foreach ($columnStyles as $columnIndex => $columnStyle) {
+            $table->setColumnStyle($columnIndex, $columnStyle);
+        }
+
+        $table->render();
     }
 
     private function setOutput()
